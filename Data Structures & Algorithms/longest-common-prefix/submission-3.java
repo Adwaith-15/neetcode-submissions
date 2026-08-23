e23e72af17f96800d0276466fc7c4200838e8b12
Adwaith Mulakala <adwaithmulakala2006@gmail.com>
2026-08-23 12:46:00 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+       
+    char[] pre= strs[0].toCharArray();
+    boolean y=true;
+    for(int i=0;i<pre.length;i++){
+      for(int j=1;j<strs.length;j++){
+        char[] pre2 = strs[i].toCharArray();
+        if(pre[i] != pre2[i]){
+          y=false;
+          break;
+        }
+        else continue;
+        
+        
+      }
+      if(y==false){
+        pre=Arrays.copyOf(pre,i);
+        y=true;
+      }
+    }
+    return new String(pre); 
+    }
+}
