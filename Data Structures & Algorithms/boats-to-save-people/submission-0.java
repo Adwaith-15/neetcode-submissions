95703e93e3d023f03c290a4e1355122915a39f83
Adwaith Mulakala <adwaithmulakala2006@gmail.com>
2026-08-26 11:32:54 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l=0;
+        int r=people.length-1,boat=0;
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                l++;
+                r--;
+                boat++;
+            }
+            else{
+                r--;
+                boat++;
+            }
+        }
+        return boat;
+    }
+}
